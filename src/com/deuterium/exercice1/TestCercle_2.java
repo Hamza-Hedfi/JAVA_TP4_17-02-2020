2e4566ca321c5fd024e70500fea5094107ca94cd
Hamza Hadfi <hedfi.hamza@hotmail.fr>
2020-02-17 14:31:40 +0100

    Update ex1

--- src/com/deuterium/exercice1/TestCercle_2.java
+++ src/com/deuterium/exercice1/TestCercle_2.java
@@ -20,10 +20,10 @@ public class TestCercle_2 {
         pt2 = new Cercle_v2(14.7, 62, 5.2);
         c1 = new Cercle_v2(3.56, 7.84, 2.25);
 
-        c2 = new Point(56.1, 76.9); // ==> c2 de type Cercle_v2 !!!
+        /* c2 = new Point(56.1, 76.9); */ // ==> c2 de type Cercle_v2 !!!
         pt1.deplace(0.5, 0.5);
         pt2.deplace(0.3, 0.8);
-        pt2.changerRayon(6.21);         // ==> pt2 n'a pas de méthode changerRayon
+        /* pt2.changerRayon(6.21); */         // ==> pt2 n'a pas de méthode changerRayon
         c1.deplace(0.4, 0.5);
         c1.changeRayon(2.52);
         c1.x = 4.78;
